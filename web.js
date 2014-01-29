438069c5c6f1a70f30e148bb8e8918566f38fe53
Michael Carter <Michael.Carter@WebFilings.com>
2014-01-29 11:07:05 -0600

    serve static directory

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var path = require('path');
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(path.join(__dirname, 'www')));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
